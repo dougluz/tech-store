4d6888f4a21fd2efb93007a414d2a10196c0bca4
dougluz <dougs.2210@gmail.com>
2020-06-12 11:26:33 -0300

    feat: added a api with json-server package

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,29 @@
 <template>
-  <div class="home"></div>
+  <section>
+    <h1>🛒 Compre ou Venda 🎉</h1>
+    <ProductSearch />
+    <ProductsList />
+  </section>
 </template>
 
 <script>
+import ProductSearch from '@/components/ProductSearch.vue';
+import ProductsList from '@/components/ProductsList.vue';
+
 export default {
   name: 'Home',
+  components: {
+    ProductSearch,
+    ProductsList,
+  },
 };
 </script>
+
+<style scoped>
+h1 {
+  font-size: 2rem;
+  color: #87f;
+  margin-top: 40px;
+  text-align: center;
+}
+</style>
