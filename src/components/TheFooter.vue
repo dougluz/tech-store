76cf9359dde87cce9c7f489e55b3f1ad7325e5cf
dougluz <dougs.2210@gmail.com>
2020-06-12 10:53:14 -0300

    feat: created the footer and the header

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,23 @@
+<template>
+  <footer>
+    <p>Tech Store - Feito com 💘 por Douglas Souza Luz</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'TheFooter',
+};
+</script>
+
+<style>
+footer {
+  background: #87f;
+  padding: 25px 0;
+  text-align: center;
+  font-size: 1.2rem;
+  color: white;
+  position: relative;
+  top: 75px;
+}
+</style>
