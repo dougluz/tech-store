329e3b3e1c41c5afefb208dea31e7ab7a559f64b
dougluz <dougs.2210@gmail.com>
2020-06-12 17:52:08 -0300

    feat: created services

--- src/components/ProductSearch.vue
+++ src/components/ProductSearch.vue
@@ -1,10 +1,21 @@
 <template>
-  <input type="text" />
+  <form action="">
+    <input type="text" name="busca" id="busca" v-model="search" />
+    <input type="submit" id="lupa" value="Buscar" @click.prevent="searchProducts" />
+  </form>
 </template>
 
 <script>
 export default {
   name: 'ProductSearch',
+  data: () => ({
+    search: '',
+  }),
+  methods: {
+    searchProducts() {
+      this.$router.push({ query: { q: this.search } });
+    },
+  },
 };
 </script>
 
--- src/components/ProductsList.vue
+++ src/components/ProductsList.vue
@@ -1,10 +1,35 @@
 <template>
-  <div></div>
+  <div>
+    <section class="produtos-container">
+      <div class="" v-for="product in products" :key="product.id">
+        <img v-if="product.fotos" :src="product.fotos[0]" :alt="product.fotos[0].titulo" />
+        <p class="preco">{{ product.preco }}</p>
+        <h2 class="titulo">{{ product.nome }}</h2>
+        <p class="">{{ product.descricao }}</p>
+      </div>
+    </section>
+    {{ url }}
+  </div>
 </template>
 
 <script>
+import service from '../services/services';
+
 export default {
   name: 'ProductsList',
+  data: () => ({
+    products: null,
+  }),
+  methods: {
+    getProducts() {
+      service.get('produto').then((response) => {
+        this.products = response.data;
+      });
+    },
+  },
+  created() {
+    this.getProducts();
+  },
 };
 </script>
 
