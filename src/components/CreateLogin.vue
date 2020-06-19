3036b8e3c41b84fd85f4a013f17278300cafc096
Douglas de Souza Luz <dougs.2210@gmail.com>
2020-06-18 22:32:08 -0300

    feat: created a form to handle the user registration

--- src/components/CreateLogin.vue
+++ src/components/CreateLogin.vue
@@ -1,13 +1,43 @@
 <template>
-  <div></div>
+  <section>
+    <h2>Crie a sua conta</h2>
+    <transition mode="out-in">
+      <button v-if="!create" class="btn" @click="create = true">Criar Conta</button>
+      <UserForm v-if="create">
+        <button class="btn btn-form">Criar Usuário</button>
+      </UserForm>
+    </transition>
+  </section>
 </template>
 
 <script>
+import UserForm from './UserForm.vue';
+
 export default {
   name: 'CreateLogin',
+  data: () => ({
+    create: false,
+  }),
+  components: {
+    UserForm,
+  },
 };
 </script>
 
-<style>
+<style scoped>
+h2 {
+  text-align: center;
+  margin-top: 40px;
+  margin-bottom: 10px;
+}
+.btn {
+  width: 100%;
+  max-width: 300px;
+  margin-left: auto;
+  margin-right: auto;
+}
 
+.btn-form {
+  max-width: 100%;
+}
 </style>
---
+++ src/components/UserForm.vue
@@ -0,0 +1,44 @@
+<template>
+  <form action="">
+    <label for="nome">Nome</label>
+    <input type="text" id="name" name="name" v-model="name">
+    <label for="email">Email</label>
+    <input type="email" id="email" name="email" v-model="email">
+    <label for="password">Senha</label>
+    <input type="password" id="password" name="password" v-model="password">
+    <label for="cep">CEP</label>
+    <input type="text" id="cep" name="cep" v-model="cep">
+    <label for="street">Rua</label>
+    <input type="text" id="street" name="street" v-model="street">
+    <label for="number">Número</label>
+    <input type="text" id="number" name="number" v-model="number">
+    <label for="neighborhood">Bairro</label>
+    <input type="text" id="neighborhood" name="neighborhood" v-model="neighborhood">
+    <label for="city">Cidade</label>
+    <input type="text" id="city" name="city" v-model="city">
+    <label for="state">Estado</label>
+    <input type="text" id="state" name="state" v-model="state">
+    <div class="button">
+      <slot></slot>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style scoped>
+form {
+  display: grid;
+  grid-template-columns: 80px 1fr;
+  align-items: center;
+}
+
+.button {
+  grid-column: 2;
+  margin-top: 10px;
+}
+</style>
